50ddec2db4fe1f2dd9b47ce726c0a786cd9c172c
Moodzs <44241827+Moodzs@users.noreply.github.com>
2018-10-17 20:58:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const now = new Map();
 //New Collection for FILES.
 bot.commands = new Discord.Collection();
 
-const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+const commandFiles = fs.readdirSync("MusicBotTest").filter(file => file.endsWith(".js"));
 
 for (const file of commandFiles) {
   const command = require (`./commands/${file}`)
